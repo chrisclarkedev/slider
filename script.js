1a3388f12df65f00cbe2c70438a603e3a7247a81
chrisclarkedev <cclarke8904@gmail.com>
2022-01-26 12:36:11 -0500

    functionality

---
+++ script.js
@@ -0,0 +1,31 @@
+const sliderContainer = document.querySelector('.slider-container');
+const slideRight = document.querySelector('.right-slide');
+const slideLeft = document.querySelector('.left-slide');
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+const slidesLength = slideRight.querySelectorAll('div').length;
+
+let activeSlideIndex = 0;
+
+// Active slide index base is 0
+// We subtract 1 from base slide length
+//Multiply that by 100 because of each slide taking 100vh
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+upButton.addEventListener('click', () => changeSlide('up'));
+downButton.addEventListener('click', () => changeSlide('down'));
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+  if (direction === 'up') {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    }
+  }
+
+  sliderRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
